e881a8c58aec8fc34d24ba3a39b8e6a7cfd24ca4
DawnSee0823 <nazarnesterov21428@gmail.com>
2023-03-19 05:15:43 -0400

    feat: create new repository

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
+
+function change_img(va) {
+    let img = document.getElementsByClassName("hi_background");
+    for(i = 0; i< img.length; i++) {
+        img[i].style.display = "none";
+    }
+    img[va-1].style.display = "block";
+}
